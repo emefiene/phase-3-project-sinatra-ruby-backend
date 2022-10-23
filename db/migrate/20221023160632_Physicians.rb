e8b524ef35ff005704917df593c308bdd7f422bd
Pius <emefiene.pius@yahoo.com>
2022-10-23 11:07:49 -0500

    physicians

---
+++ db/migrate/20221023160632_Physicians.rb
@@ -0,0 +1,10 @@
+class Physicians < ActiveRecord::Migration[6.1]
+  def change
+    create_table :physicians do |t|
+      t.string :image_url
+      t.string :name
+      t.string :specialty
+      t.integer :phone
+   end
+  end
+end
