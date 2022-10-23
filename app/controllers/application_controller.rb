0aa26deb436771d5ab8b89132f1b82df1ab1aafa
Pius <emefiene.pius@yahoo.com>
2022-10-23 11:26:05 -0500

    updated patient

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,40 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+    
+  get "/patients" do
+    patients = Patient.all
+    patients.to_json
+  end
+  
+  get "/patients/:id" do
+    patients = Patient.find(params[:id])
+    patients.to_json
   end
 
+  
+  post "/patients" do
+    patients = Patient.create(get_params)
+    patients.to_json
+  end
+  
+  
+  patch "/patients/:id" do
+    patients = Patient.find(params[:id])
+    patients.update(get_params)
+    patients.to_json
+  end
+  
+
+  delete "/patients/:id" do
+    
+    patients = Patient.find(params[:id])
+    patients.destroy
+  end
+
+  def get_params
+    {name:params[:name], email:params[:email], address:params[:address], age:params[:age], phone:params[:phone]}
+  end
 end
+
+
