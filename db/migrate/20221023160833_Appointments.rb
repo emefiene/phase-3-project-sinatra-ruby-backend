0bea812b8b1e48c2bbfd84a8c16e8a6e905d9346
Pius <emefiene.pius@yahoo.com>
2022-10-23 11:09:23 -0500

    update appoitment

---
+++ db/migrate/20221023160833_Appointments.rb
@@ -0,0 +1,10 @@
+class Appointments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :appointments do |t|
+      t.datetime :time
+      t.datetime :date
+      t.integer :patient_id
+      t.integer :physician_id
+    end
+  end
+end
